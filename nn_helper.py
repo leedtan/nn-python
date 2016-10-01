added Y normalization, normalizes to [0,1] or [-1,1] depending on last transfer function. Might have bug in normalization code, tanh transfer function or test_sine code because test_sine error is surprisingly high.

nn_helper.py
@@ -12,7 +12,20 @@ def sigmoid(x):
 def sigmoid_p(x):
     return sigmoid(x) * (1.0 - sigmoid(x))
 
-trans_fcns = {'sigmoid': [sigmoid, sigmoid_p]}
+
+def tanh(x):
+    return np.tanh(x)
+
+
+def tanh_p(x):
+    return 1.0 - x ** 2
+
+
+trans_fcns = {
+              'sigmoid': [sigmoid, sigmoid_p],
+              'tanh': [tanh, tanh_p]
+              }
+
 
 def mse(Y, a):
     return np.sum(np.square(Y - a))
@@ -48,7 +61,6 @@ def denormalize(vect, stdev, offset):
 
 
 def print_y(y_predict, Y, dec = 2):
-    print (np.mean(np.square(y_predict - Y)))
     if y_predict.ndim > 1:
         y_predict = y_predict.flatten()
     print (np.around(y_predict, dec))

nn_python.py
@@ -1,8 +1,4 @@
 import sys
-#import pandas as pd
-#import numpy as np
-#import matplotlib
-#import matplotlib.pyplot as plt
 import copy
 from nn_helper import *
 from array import array
@@ -12,7 +8,7 @@ class nnp(object):
 
     def __init__(self, layers, trans='sigmoid', perf_fcn='mse',
                  reg=0, netstruc='ff'):
-        self.y_norm = np.ones(layers[-1])
+        self.Yscale, self.Yoffset = [0,0]
         self.best_perf = np.inf
         self.layers = layers
         self.reg = reg
@@ -52,6 +48,13 @@ class nnp(object):
             print("Error: " + str(trans) + " not known in tran_fcns")
             sys.exit(0)
                         
+    def _set_trans_list(self,trans):
+        if len(trans) < len(self.layers) - 1:
+            print('wrong number of transfer functions listed')
+            sys.exit(0)
+        for idx in range(1, len(self.layers)):
+            self._set_one_trans(trans, idx)
+            
     def _settrans(self, trans):
         self.trans = [0] * (len(self.layers))
         self.trans_p = [0] * (len(self.layers))
@@ -59,11 +62,7 @@ class nnp(object):
             for idx in range(1, len(self.layers)):
                 self._set_one_trans(trans, idx)
         elif isinstance(trans, list):
-            if len(trans) < len(self.layers) - 1:
-                print('wrong number of transfer functions listed')
-                sys.exit(0)
-            for idx in range(1, len(self.layers)):
-                self._set_one_trans(trans, idx)
+            self._set_trans_list(trans)
                 
     def _initLmap(self, netstruc):
         if isinstance(netstruc, str):
@@ -192,7 +191,30 @@ class nnp(object):
             self.LR = LR
             for _ in range(re_init_d):
                 self._re_init_once(X, Y)
-                
+             
+    
+    def _calc_norm_Y(self, Y):
+        scale = np.amax(Y) - np.amin(Y)
+        Y = Y / scale
+        offset = np.amin(Y)
+        Y = Y - offset
+        #At this point, Y is [0,1]
+        if self.trans[-1] in [sigmoid]:
+            pass
+        if self.trans[-1] in [tanh]:
+            scale = scale / 2
+            offset += 0.5
+        return scale, offset
+    
+    
+    def _normalize_Y(self, Y):
+        return (Y / self.Yscale) - self.Yoffset
+    
+    
+    def _denormalize_Y(self, Y):
+        return (Y + self.Yoffset) * self.Yscale
+    
+    
     def _prepare_training(self,X,Y,LR,batch_type,verb):
         X = self._verify_x(X)
         if isinstance(X, bool):
@@ -205,8 +227,9 @@ class nnp(object):
         self.samples = X.shape[0]
         self.verb = verb
         self.LR = LR
-        self.y_norm = np.amax(np.abs(np.vstack((self.y_norm, Y))))
-        Y = Y / self.y_norm
+        if self.Yscale == 0:
+            self.Yscale, self.Yoffset = self._calc_norm_Y(Y)
+        Y = self._normalize_Y(Y)
         self._assign_batches(batch_type)
         self.best_weights = copy_weights(self.weights)
         self.gb_weights = copy_weights(self.best_weights)
@@ -230,8 +253,10 @@ class nnp(object):
         x = apply_norm(x, self.Xstd, self.Xoffset)
         self.samples = x.shape[0]
         output = np.empty([self.samples, self.act_vals[-1].shape[0]])
+        #this can be made more efficient through matrix math.
         for d_idx in range(self.samples):
             self.act_vals[0] = np.atleast_1d(x[d_idx])
             self._prop_fwd()
             output[d_idx] = self.act_vals[-1]
-        return output * self.y_norm
+        output = self._denormalize_Y(output)
+        return output

nn_test.py
@@ -1,5 +1,6 @@
 from nn_python import *  # noqa
-
+import time
+import matplotlib.pyplot as plt
 def test_xor(verb=0, re_init=10, netstruc='ff'):
     tNN = nnp([2, 100, 1], reg=10 ** -10, netstruc=netstruc)
     X = np.array([[0, 0],
@@ -14,5 +15,44 @@ def test_xor(verb=0, re_init=10, netstruc='ff'):
         print_y(y_predict, Y)
     return np.mean(np.square(y_predict - Y))
 np.random.seed(1)
-for _ in range(100):
-    test_xor()
+test_xor()
+    
+    
+def test_xor3(verb=0, netstruc='ff'):
+    tNN = nnp([3, 500, 1], reg= 10 ** -1)
+    X = np.array([[0, 0, 0],
+                  [0, 1, 0],
+                  [1, 0, 0],
+                  [1, 1, 0],
+                  [0, 0, 1],
+                  [0, 1, 1],
+                  [1, 0, 1],
+                  [1, 1, 1]])
+    Y = np.array([np.array([x[0] ^ x[1] ^ x[2]]) for x in X])
+    if verb > -1:
+        print(Y.flatten())
+    tNN.train(X, Y, epochs=100, verb=verb, re_init=10, re_init_d=20)
+    y_predict = tNN.predict(X)
+    if verb > -1:
+        print_y(y_predict, Y)
+    return np.mean(np.square(y_predict - Y))
+test_xor3()
+
+
+#Does not yet work well.
+def test_sine(verb=0, netstruc='ff'):
+    tNN = nnp([1, 100, 1], reg= 10 ** -5, trans='tanh')
+    X = np.array([np.array([x]) for x in np.arange(0, 2 * np.pi, .1)])
+    Y = np.array([np.sin(x) + np.random.randn(x.shape[0]) * .001
+                      for x in X])
+    tNN.train(X, Y, epochs=100, verb=verb, re_init=3, re_init_d=10)
+    y_predict = tNN.predict(X)
+    if verb > 0:
+        print(Y.flatten())
+    if verb > -1:
+        print_y(y_predict, Y)
+    return np.mean(np.square(y_predict - Y))
+if 1:
+    sine_error = test_sine(verb = 0)
+    print('Sine error is: ', sine_error)
+