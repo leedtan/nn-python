added dot product test. Realized Y-normalization bug, fixed in class function

nn_python.py
@@ -227,7 +227,7 @@ class nnp(object):
                           batch_type, verb, nudge, objective, del_thresh):
         X = self._verify_x(X)
         if isinstance(X, bool):
-            print("X size %d, need %d" % (X.shape[1], self.layers[0]))
+            print("X size %d, need %d" % (X.shape[0], self.layers[0]))
             return
         if self.Xstd == []:
             X, self.Xstd, self.Xoffset = helper.normalize(X)
@@ -249,7 +249,7 @@ class nnp(object):
         self.gb_weights = helper.copy_weights(self.best_weights)
         self.gb_perf = np.copy(self.best_perf)
         self.gb_LR = self.LR
-        return X
+        return X, Y
 
 #This is a very complicated function. Essentially if the
 #optimization is stuck at a flat area, this should nudge it around randomly
@@ -287,7 +287,7 @@ class nnp(object):
     def train(self, X, Y, LR=1, batch_type=helper.GROUP, verb=0,
               re_init=3, re_init_d=10, epochs = 10,
               nudge = 0, objective = 0, del_thresh=0, max_fail = np.inf):
-        X = self._prepare_training(X, Y, LR, batch_type, verb,
+        X, Y = self._prepare_training(X, Y, LR, batch_type, verb,
                                    nudge, objective, del_thresh)
         self._init_k_times(X, Y, re_init, re_init_d, LR)
         self.best_perf = self.gb_perf

nn_test.py
@@ -21,7 +21,7 @@ np.random.seed(2)
 #test_xor()
 
 
-def test_xor3(verb=0, netstruc='ff', **kwargs):
+def test_xor3(verb=0, **kwargs):
     tNN = nn_p.nnp([3, 1000, 1], reg=10 ** -10)
     X = np.array([[0, 0, 0],
                   [0, 1, 0],
@@ -59,7 +59,25 @@ def test_sine(verb=0, netstruc='ff', **kwargs):
     if verb > -1:
         helper.print_y(y_predict, Y)
     return np.mean(np.square(y_predict - Y))
-if 1:
+if 0:
     sine_error = test_sine(verb = 1, objective = 10**-10, del_thresh=10**-15,
                            max_fail = 100, nudge = 100)
     print('Sine error is: ', sine_error)
+
+#need to visualize results
+def x1dotx2(verb=0, **kwargs):
+    tNN = nn_p.nnp([2, 100, 1], reg=10 ** -10, trans = 'tanh')
+    X = np.array([np.array([x, c]) for x in np.linspace(-1, 1, 7)
+                 for c in np.linspace(-5, 5, 25)])
+    Y = np.array([np.array([x[0] * x[1]]) for x in X])
+    tNN.train(X, Y, epochs=1000, verb=verb, re_init=3, re_init_d=10, **kwargs)
+    y_predict = tNN.predict(X)
+    if verb > 0:
+        print(Y.flatten())
+    if verb > -1:
+        helper.print_y(y_predict, Y)
+    return np.mean(np.square(y_predict - Y))
+if 1:
+    dot_err = x1dotx2(objective = 10**-10, del_thresh=10**-15, max_fail = 100, nudge = 100,
+            verb = 0)
+    print('dot error is: ', dot_err)